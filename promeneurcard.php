36464f8615a7be9c807f9e3bc9fbcb227f2fa8fe
R0mpo <r.pohyer@gmail.com>
2019-01-16 15:37:29 +0100

    tete

--- promeneurcard.php
+++ promeneurcard.php
@@ -1,7 +1,12 @@
 <?php
 // Initialize the session
 session_start();
-?>
+
+// Check if the user is logged in, if not then redirect to login page
+if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
+    header("location: login.php");
+    exit;
+} ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
