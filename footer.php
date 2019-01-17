2e7a3611d213e8f3cf4b8fa1d85cc0acfd4847a9
R0mpo <r.pohyer@gmail.com>
2019-01-17 21:38:33 +0100

    fix de derniere minute

--- footer.php
+++ footer.php
@@ -1,17 +1,44 @@
   <!-- Footer avec liens de navigation Pied de Page -->
+  <head>
+      <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+    <title>dogpal.fr</title>
+  </head>
+
   <footer>
     <div class="container centered mt-3">
       <div class="row mt-4" id="piedPage">
-        <div class="col-md-6" id="piedPageGauche">
-          <ul class="nav navbar-nav">
-            <li><a href="index.html">Acceuil</a></li>
-            <li><a href="prix.html">Tarifs</a></li>
-            <li><a href="https://medium.com/search?q=dog%20walking" target="_blank">Blog</a></li>
-          </ul>
-        </div>
+        <div class="col-md-6 id="piedPageGauche">
+          <div class="row mt-4 mb-4">
+                <?php
+                  if (isset($_SESSION["loggedin"])){
+
+                    echo '<div class="col-6 mb-4">';
+                    echo '<ul class="nav navbar-nav">';
+                    echo '<li><a href="index.html">Acceuil</a></li>';
+                    echo '<li><a href="prix.html">Tarifs</a></li>';
+                    echo '<li><a href="https://medium.com/search?q=dog%20walking target="_blank">Blog</a></li></ul></div>';
+
+                    echo '<div class="col-6">';
+                    echo '<ul class="nav navbar-nav">';
+                    echo '<li><a href="promeneurcard.php">Promeneurs</a></li>'; 
+                    echo '<li><a href="owner.php">Missions</a></li>';
+                    echo '<li"><a href="createMission.php">Dépot d\'annonce</a></li>'; 
+                    echo '</ul></div>';
+                  }else{
+                    echo '<div class="col-md-6 text-center">';
+                    echo '<ul class="nav navbar-nav">';
+                    echo '<li><a href="index.html">Acceuil</a></li>';
+                    echo '<li><a href="prix.html">Tarifs</a></li>';
+                    echo '<li><a href="https://medium.com/search?q=dog%20walking target="_blank">Blog</a></li></ul></div>';
+                  }
+                ?>
+            </div>
+          </div>
         
-        <div class="col-md-6">
-          <iframe width="560" height="315" src="https://www.youtube.com/embed/I11bPV4pgEk" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"></iframe>
+        <div class="col-md-6 vanish centered">
+          <iframe width="400" height="220" src="https://www.youtube.com/embed/I11bPV4pgEk" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"></iframe>
         </div>
       </div>
     </div>
